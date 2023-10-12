d50400235948f053faa54d290c46ef8accac3555
Dhanush MC <dhanushmc93@gmail.com>
2023-10-12 23:14:47 +0530

    commit

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ import { MongoClient } from "mongodb";
 import practiceRoute from "./routes/practice.route.js";
 import * as dotenv from "dotenv";
 import cors from "cors";
-dotenv.config();
 
+dotenv.config();
+// console.log(process.env);  // evn -> environment variables
+
+// STEP-2
 export const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 
+// STEP-3
+// const PORT = 4503;
 app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊🤩");
 });
 
+// last step 3rd party CORS Middleware
 app.use(cors());
 
 const MONGO_URL = process.env.MONGO_URL;
@@ -22,6 +28,7 @@ console.log("Mongo is connected 👍");
 
 app.use("/practice", practiceRoute);
 
+// STEP-4
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
 
 export { client };
--- routes/practice.route.js
+++ routes/practice.route.js
@@ -3,7 +3,10 @@ import express from "express";
 import { ObjectId } from "mongodb";
 const router = express.Router();
 
+// /movies - display all movie data
+// while sending - converts to JSON
 router.get("/", async function (req, res) {
+  // db.movies.find({ language : "tamil" , name : "Vikram" })  // gets movie from local
   console.log(req.query);
   const datas = await client
     .db("practice-data")
@@ -13,8 +16,10 @@ router.get("/", async function (req, res) {
   res.send(datas);
 });
 
+// /movies/:id
 router.get("/:id", async function (req, res) {
   const { id } = req.params;
+  // use find instead of filter to get only object
   console.log(id);
   const result = await client
     .db("practice-data")
@@ -26,10 +31,13 @@ router.get("/:id", async function (req, res) {
     : res.status(404).send({ message: "data not found" });
 });
 
+// update
 router.put("/:id", async function (req, res) {
   const { id } = req.params;
   console.log(id);
   const data = req.body;
+  //   db.movies.updatOne({id:"103"})
+  // const movie = movies.find((mv) => mv.id == id)
   const result = await client
     .db("practice-data")
     .collection("data")
@@ -40,9 +48,11 @@ router.put("/:id", async function (req, res) {
     : res.status(404).send({ message: "Invaild token" });
 });
 
+// Create
 router.post("/", async function (req, res) {
   const datas = req.body;
   console.log(datas);
+  // db.movies.insertOne(data)
   const result = await client
     .db("practice-data")
     .collection("data")
@@ -50,9 +60,11 @@ router.post("/", async function (req, res) {
   res.send(result);
 });
 
+// delete
 router.delete("/:id", async function (req, res) {
   const { id } = req.params;
   console.log(id);
+  // db.movies.deletone(data)({id:101})
   const data = await client
     .db("practice-data")
     .collection("data")
