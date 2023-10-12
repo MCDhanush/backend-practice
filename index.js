11148a034977f079184de3aa9c3d786811dae07a
Dhanush MC <dhanushmc93@gmail.com>
2023-10-12 21:32:31 +0530

    commit

--- index.js
+++ index.js
@@ -1,101 +1,88 @@
-import express from "express"
-import { MongoClient} from "mongodb";
-import bcrypt from "bcrypt"
+import express from "express";
+import { MongoClient } from "mongodb";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import practiceRoute from "./routes/practice.route.js"
-import * as dotenv from "dotenv"
-dotenv.config()
+import practiceRoute from "./routes/practice.route.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export const app = express();
 const PORT = process.env.PORT;
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", function (request, response) {
-    response.send("🙋‍♂️, 🌏 🎊🤩");
+  response.send("🙋‍♂️, 🌏 🎊🤩");
 });
 
-const MONGO_URL = process.env.MONGO_URL
-const client = new MongoClient(MONGO_URL)
-await client.connect() // top level await we use await anywhere
+const MONGO_URL = process.env.MONGO_URL;
+const client = new MongoClient(MONGO_URL);
+await client.connect(); // top level await we use await anywhere
 console.log("Mongo is connected 👍");
 
-app.use("/practice",practiceRoute)
+app.use("/practice", practiceRoute);
 
-
-async function getHashedPassword(password){
-    const NO_OF_ROUNDS = 10;
-    const salt = await bcrypt.genSalt(NO_OF_ROUNDS)
-    const hashedPassword = await bcrypt.hash(password,salt)
-    return hashedPassword
+async function getHashedPassword(password) {
+  const NO_OF_ROUNDS = 10;
+  const salt = await bcrypt.genSalt(NO_OF_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
 }
 
-app.post("/signup",async function(req,res){
-const{username,password}= req.body
-console.log(password)
+app.post("/signup", async function (req, res) {
+  const { username, password } = req.body;
+  console.log(password);
 
-const userFromDB = await client.db("practice-data").collection("users").findOne({username:username})
-console.log(userFromDB)
-if(userFromDB){ //1
-    res.status(404).send({message:"user already exits"})
-}else if(password.lenght < 8){ //2
-    res.status(401).send({message:"password must be 8 character"})
-}else{
-    const hashedPassword =await getHashedPassword(password) //3.1
-    console.log(password,hashedPassword);
+  const userFromDB = await client
+    .db("practice-data")
+    .collection("users")
+    .findOne({ username: username });
+  console.log(userFromDB);
+  if (userFromDB) {
+    //1
+    res.status(404).send({ message: "user already exits" });
+  } else if (password.lenght < 8) {
+    //2
+    res.status(401).send({ message: "password must be 8 character" });
+  } else {
+    const hashedPassword = await getHashedPassword(password); //3.1
+    console.log(password, hashedPassword);
 
-const result = await client.db("practice-data").collection("users").insertOne({  //3.2
-    username :username,
-    password: hashedPassword
-})
-res.send(result)
+    const result = await client
+      .db("practice-data")
+      .collection("users")
+      .insertOne({
+        //3.2
+        username: username,
+        password: hashedPassword,
+      });
+    res.send(result);
+  }
+});
 
-}})
+app.post("/login", async function (req, res) {
+  const { username, password } = req.body;
+  // console.log(password)
 
-
-app.post("/login",async function(req,res){
-    const {username,password} = req.body
-    // console.log(password)
-   
-    const userFromDB =await client.db("practice-data").collection("users").findOne({username:username})
-    console.log(userFromDB,password)
-    if(!userFromDB){
-        res.status(404).send({message:"Invalid user"})
-    }else{
-        const sortedDBPassword = userFromDB.password
-        const isPasswordMatch = await bcrypt.compare(password,sortedDBPassword)
-        console.log(sortedDBPassword)
-        if(isPasswordMatch){
-            const token = jwt.sign({id:userFromDB._id},process.env.SECRECT_KEY)
-            res.send({message:"succesfully logined",token:token})
-        }else{
-            res.status(401).send({message:"invalid credantials"})
-        }
+  const userFromDB = await client
+    .db("practice-data")
+    .collection("users")
+    .findOne({ username: username });
+  console.log(userFromDB, password);
+  if (!userFromDB) {
+    res.status(404).send({ message: "Invalid user" });
+  } else {
+    const sortedDBPassword = userFromDB.password;
+    const isPasswordMatch = await bcrypt.compare(password, sortedDBPassword);
+    console.log(sortedDBPassword);
+    if (isPasswordMatch) {
+      const token = jwt.sign({ id: userFromDB._id }, process.env.SECRECT_KEY);
+      res.send({ message: "succesfully logined", token: token });
+    } else {
+      res.status(401).send({ message: "invalid credantials" });
     }
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  }
+});
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
 
-export{client}
+export { client };
--- routes/practice.route.js
+++ routes/practice.route.js
@@ -1,46 +1,65 @@
 import { client } from "../index.js";
-import express from "express"
+import express from "express";
 import { ObjectId } from "mongodb";
-const router= express.Router()
+const router = express.Router();
 
 router.get("/", async function (req, res) {
-    console.log(req.query);
-    const datas = await client.db("practice-data").collection("data").find({}).toArray();
-    res.send(datas);
+  console.log(req.query);
+  const datas = await client
+    .db("practice-data")
+    .collection("data")
+    .find({})
+    .toArray();
+  res.send(datas);
 });
 
+router.get("/:id", async function (req, res) {
+  const { id } = req.params;
+  console.log(id);
+  const result = await client
+    .db("practice-data")
+    .collection("data")
+    .findOne({ _id: new ObjectId(id) });
+  console.log(result);
+  result
+    ? res.send(result)
+    : res.status(404).send({ message: "data not found" });
+});
 
-router.get("/:id",async function(req,res){
-    const {id} = req.params;
-    console.log(id);
-    const result = await client.db("practice-data").collection("data").findOne({_id:new ObjectId(id)})
-    console.log(result)
-    result ? res.send(result):res.status(404).send({message : "data not found"})
-})
+router.put("/:id", async function (req, res) {
+  const { id } = req.params;
+  console.log(id);
+  const data = req.body;
+  const result = await client
+    .db("practice-data")
+    .collection("data")
+    .updateOne({ _id: new ObjectId(id) }, { $set: data });
+  console.log(result);
+  result
+    ? res.send(result)
+    : res.status(404).send({ message: "Invaild token" });
+});
 
-router.put("/:id",async function(req,res){
-    const {id} = req.params
-    console.log(id)
-    const data = req.body
-    const result =await client.db("practice-data").collection("data").updateOne({_id:new ObjectId(id)},{$set:data})
-    console.log(result)
-    result ? res.send(result) : res.status(404).send({message:"Invaild token"})
-})
+router.post("/", async function (req, res) {
+  const datas = req.body;
+  console.log(datas);
+  const result = await client
+    .db("practice-data")
+    .collection("data")
+    .insertOne(datas);
+  res.send(result);
+});
 
+router.delete("/:id", async function (req, res) {
+  const { id } = req.params;
+  console.log(id);
+  const data = await client
+    .db("practice-data")
+    .collection("data")
+    .deleteOne({ _id: new ObjectId(id) });
+  data
+    ? res.send({ data })
+    : res.status(404).send({ message: "invalid token" });
+});
 
-
-router.post("/",async function(req,res){
-    const datas = req.body
-    console.log(datas)
-    const result =await client.db("practice-data").collection("data").insertOne(datas)
-    res.send(result)
-})
-
-router.delete("/:id",async function(req,res){
- const {id} = req.params
- console.log(id)
- const data =await client.db("practice-data").collection("data").deleteOne({_id:new ObjectId(id)})
- data ? res.send({data}) : res.status(404).send({message:"invalid token"})
-})
-
-export default router
+export default router;
